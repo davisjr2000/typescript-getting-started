e72186db313907726b95b311dca78faa64a16333
Davis JR <davisjr2000@hotmail.com>
2020-04-20 21:32:41 -0500

    Add more functions

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,30 @@
 function startGame() {
 
-  let playerName :string = 'Davis';
+  let playerName :string | undefined = getInputValue('playername');
   logPlayer(playerName);
 
-  var messagesElement = document.getElementById('messages');
-  messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+  postScore(100, playerName);
+
 }
 
-function logPlayer(name : any){
-  console.log(`New game starting for player: ${name}`)
+function logPlayer(name : string = 'MultiMath Player'): void{
+  console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementID: string) : string | undefined {
+
+  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+  if (inputElement.value === '') {
+    return undefined;
+  } else {
+    return inputElement.value;
+  }
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+   const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+   scoreElement!.innerText = `${score} - ${playerName}`;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
